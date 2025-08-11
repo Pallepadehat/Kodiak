c95cf4a530d9197443b419bf8b9697e8cd12f90b
Pallepadehat <patrick.jakobsen@icloud.com>
2025-08-11 21:49:22 +0200

    Initial Commit

---
+++ Kodiak/KodiakApp.swift
@@ -0,0 +1,17 @@
+//
+//  KodiakApp.swift
+//  Kodiak
+//
+//  Created by Patrick Jakobsen on 11/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct KodiakApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
